137e25f54c905f5f82d7c3ced28cf0819612ad0b
Thomas <88990664+bigezwest@users.noreply.github.com>
2024-06-19 18:27:36 +0200

    test 1: Repeat string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let returnString = "";
+    while (num > 0) {
+        returnString += str;
+        num--;
+    }
+    return returnString;
 };
 
+// repeatString("hey", 3);
 // Do not edit below this line
 module.exports = repeatString;
