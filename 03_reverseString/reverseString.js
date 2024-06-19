12e0ebff970af70734d1668c13608c1c704636fd
Thomas <88990664+bigezwest@users.noreply.github.com>
2024-06-19 21:19:43 +0200

    Test 1: Reverse single word

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const originalString = string;
+    const originalString_len = string.length;
+    let reversedString = "";
 
+    for (let i = originalString_len - 1; i >= 0; i--) {
+        reversedString += originalString[i];
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
