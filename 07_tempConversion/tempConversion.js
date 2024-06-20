def62bd049fc05ead9d40bb14c6dddb788f3e37a
Thomas <88990664+bigezwest@users.noreply.github.com>
2024-06-20 14:08:58 +0200

    feature(convertToCelsius): Add formula
    * Add formula to convert to convertToCelsius
    * Run Test 1

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  const tempC = (tempF - 32) * (5 / 9);
+  return tempC;
 };
 
-const convertToFahrenheit = function() {
-};
+const convertToFahrenheit = function(temp) {
+  // const f = temp * ( 9 / 5) + 32;
+  // return f;
+}
 
 // Do not edit below this line
 module.exports = {
