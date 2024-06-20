1648bc23f41eeab99a0822676d6731eab4f4cd27
Thomas <88990664+bigezwest@users.noreply.github.com>
2024-06-20 07:18:07 +0200

    feature(removeFromArray): Remove one item

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, paramsToRemove) {
+
+    const arrayFiltered = arr.filter(function (item) {
+
+            return item !== paramsToRemove;
+    });
+
+    return arrayFiltered;
+
 };
 
 // Do not edit below this line
