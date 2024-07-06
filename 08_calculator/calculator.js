28ab3c988a566280481f81c2020ce24c1f5f0d07
Thomas <88990664+bigezwest@users.noreply.github.com>
2024-07-06 06:44:16 +0200

    feature(multiply): Add function logic

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -12,8 +12,10 @@ const sum = function(arr) {
   }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((accumulator, current) => {
+    return accumulator *= current;
+  }, 1);
 };
 
 const power = function() {
