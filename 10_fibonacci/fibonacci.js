efdd700e11226bdf8f24b8a3178f6117c0398e6b
Thomas <88990664+bigezwest@users.noreply.github.com>
2024-07-07 04:43:48 +0200

    feature(fibonacci): Add block committed
    * Build the sequence [1, 1, 2,...].  Return last element
    * Run first test

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(member) {
+    let returnValue = [];
 
+    for (let i = 0; i < member; i++) {
+        if (i <= 1) {
+            returnValue.push(1);
+        } else {
+            returnValue.push(i + returnValue[i - 1]);
+        }
+    }
+    return (returnValue.length - 1);
 };
 
 // Do not edit below this line
